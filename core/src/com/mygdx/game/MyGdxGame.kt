b21d264e49e898785ce5b080b8aa884bc3dc4ab8
akashKarmakar02 <akash.anan03@gmail.com>
2023-10-11 18:22:18 +0530

    added alien bullet group last shot and cooldown period for aliens to shot bullet

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.ScreenUtils
 import com.mygdx.game.Group.SpriteGroup
+import kotlin.random.Random
 
 class MyGdxGame : ApplicationAdapter() {
     private lateinit var backgroundImage: Texture
@@ -14,7 +15,10 @@ class MyGdxGame : ApplicationAdapter() {
     private lateinit var spaceship: Spaceship
     private lateinit var bulletGroup: SpriteGroup<Bullets>
     private lateinit var alienGroup: SpriteGroup<Alien>
+    private lateinit var alienBulletGroup: SpriteGroup<AlienBullet>
     private lateinit var font: BitmapFont
+    private var lastAlienShot = System.currentTimeMillis()
+    private var alienCooldown = 1000
 
     override fun create() {
         backgroundImage = Texture("img/bg.png")
@@ -22,6 +26,7 @@ class MyGdxGame : ApplicationAdapter() {
         spaceship = Spaceship(Gdx.graphics.width.toFloat() / 2, 100f, 5)
         bulletGroup = SpriteGroup()
         alienGroup = SpriteGroup()
+        alienBulletGroup = SpriteGroup()
         font = BitmapFont()
 
         createAlien()
@@ -36,6 +41,13 @@ class MyGdxGame : ApplicationAdapter() {
 
 
         batch.begin()
+        val timeNow = System.currentTimeMillis()
+        if (timeNow - lastAlienShot > alienCooldown) {
+            val attackingAlien = alienGroup.sprites[Random.nextInt(alienGroup.sprites.size)]
+            val alienBullet = AlienBullet(attackingAlien.sprite.x + attackingAlien.sprite.width / 2, attackingAlien.sprite.y)
+            alienBulletGroup.add(alienBullet)
+            lastAlienShot = timeNow
+        }
 
         batch.draw(backgroundImage, 0f, 0f)
 
@@ -43,6 +55,7 @@ class MyGdxGame : ApplicationAdapter() {
         spaceship.handleAttack(batch, bulletGroup)
         bulletGroup.render(batch)
         alienGroup.render(batch)
+        alienBulletGroup.render(batch)
 
         font.color.set(1f, 1f, 1f, 1f)
         font.region.texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
@@ -57,6 +70,7 @@ class MyGdxGame : ApplicationAdapter() {
         bulletGroup.update()
         alienGroup.update()
         spaceship.move()
+        alienBulletGroup.update()
     }
 
     private fun createAlien() {
